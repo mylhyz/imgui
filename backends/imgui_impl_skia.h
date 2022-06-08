645b3d4cc96a4589d541a915a3d5449b928e029b
mylhyz <mylhyz@gmail.com>
2022-06-08 15:39:34 +0800

    [demo]add skia c++ demo

--- backends/imgui_impl_skia.h
+++ backends/imgui_impl_skia.h
@@ -1,4 +1,10 @@
 #ifndef __IMGUI_IMPL_SKIA_H__
 #define __IMGUI_IMPL_SKIA_H__
 
+#include "include/core/SkCanvas.h"
+#include "include/core/SkPaint.h"
+#include "include/core/SkPath.h"
+#include "include/core/SkPathBuilder.h"
+#include "include/core/SkSurface.h"
+
 #endif //__IMGUI_IMPL_SKIA_H__
