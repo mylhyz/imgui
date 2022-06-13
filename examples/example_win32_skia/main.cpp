91b9a61b295cb11edccfe362622d70dd3c3030ad
mylhyz <mylhyz@gmail.com>
2022-06-13 14:59:50 +0800

    add win32 application demo

---
+++ examples/example_win32_skia/main.cpp
@@ -0,0 +1,88 @@
+#include <Windows.h>
+//
+//  函数: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  目标: 处理主窗口的消息。
+//
+//  WM_COMMAND  - 处理应用程序菜单
+//  WM_PAINT    - 绘制主窗口
+//  WM_DESTROY  - 发送退出消息并返回
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam,
+                         LPARAM lParam) {
+  switch (message) {
+    case WM_COMMAND: {
+      int wmId = LOWORD(wParam);
+      // 分析菜单选择:
+      switch (wmId) {
+        default:
+          return DefWindowProc(hWnd, message, wParam, lParam);
+      }
+    } break;
+    case WM_PAINT: {
+      PAINTSTRUCT ps;
+      HDC hdc = BeginPaint(hWnd, &ps);
+      // TODO: 在此处添加使用 hdc 的任何绘图代码...
+      EndPaint(hWnd, &ps);
+    } break;
+    case WM_DESTROY:
+      PostQuitMessage(0);
+      break;
+    default:
+      return DefWindowProc(hWnd, message, wParam, lParam);
+  }
+  return 0;
+}
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                      _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine,
+                      _In_ int nCmdShow) {
+  UNREFERENCED_PARAMETER(hPrevInstance);
+  UNREFERENCED_PARAMETER(lpCmdLine);
+
+  WCHAR szTitle[] = L"Dear ImGui Win32+Skia Example";
+  WCHAR szWindowClass[] = L"Windows";
+
+  //   注册窗口信息
+  WNDCLASSEXW wcex;
+
+  wcex.cbSize = sizeof(WNDCLASSEX);
+
+  wcex.style = CS_HREDRAW | CS_VREDRAW;
+  wcex.lpfnWndProc = WndProc;
+  wcex.cbClsExtra = 0;
+  wcex.cbWndExtra = 0;
+  wcex.hInstance = hInstance;
+  wcex.hIcon = 0;
+  wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+  wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+  wcex.lpszMenuName = 0;
+  wcex.lpszClassName = szWindowClass;
+  wcex.hIconSm = 0;
+
+  RegisterClassExW(&wcex);
+
+  //   创建窗口
+
+  HWND hWnd =
+      CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, CW_USEDEFAULT,
+                    0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+
+  if (!hWnd) {
+    return FALSE;
+  }
+
+  ShowWindow(hWnd, nCmdShow);
+  UpdateWindow(hWnd);
+
+  MSG msg;
+
+  // 主消息循环:
+  while (GetMessage(&msg, nullptr, 0, 0)) {
+    TranslateMessage(&msg);
+    DispatchMessage(&msg);
+  }
+
+  return (int)msg.wParam;
+}
